d368016743bd051078b62dd5943a2263c009593e
generalkang05 <kangsooha@naver.com>
2024-01-10 11:30:01 +0900

    spaceshooting 게임 완성

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import time
+from datetime import datetime
 # 1. 게임 초기화
 pygame.init()
 
@@ -36,6 +38,16 @@ class obj:
     def show(self):
         screen.blit(self.image, (self.x, self.y))
 
+def crash(a, b):
+    if (a.x - b.x_size <= b.x) and (b.x <= a.x + a.x_size):
+        if (a.y - b.y_size <= b.y) and (b.y <= a.y + a.y_size):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 fighter = obj()
 fighter.put_image("img/fighter.png")
 fighter.change_size(50, 80)
@@ -52,9 +64,26 @@ rock_list = []
 
 color = (0, 0, 0)
 k = 0
-rock01 = pygame.image.load("img/rock01.png").convert_alpha()
 
+GO = 0
+kill = 0
+loss = 0
+
+# 4-0 게임 시작 대기 화면 
+running = True
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                running = False
+    screen.fill(color)
+    font = pygame.font.Font("C:/Windows/Fonts/ariblk.ttf", 15)
+    text = font.render("PRESS SPACE KEY TO START THE GAME", True, (255, 255, 255))
+    screen.blit(text, (40, SCREEN_HEIGHT / 2 - 50))
+    pygame.display.flip()
 # 4. 메인 이벤드
+start_time = datetime.now()
 running = True
 while running:
     # 4-1. FPS 설정
@@ -82,6 +111,11 @@ while running:
             elif event.key == pygame.K_SPACE:
                 space_go = False
     # 4-3. 입력, 시간에 따른 변화
+    now_time = datetime.now()
+    delta_time = round((now_time - start_time).total_seconds())      
+
+
+
     if left_go == True:
         fighter.x -= fighter.move
         if fighter.x <= 0:
@@ -128,6 +162,32 @@ while running:
     
     for d in d_list:
         del rock_list[d]
+        loss += 1
+
+    dm_list = []
+    dr_list = []
+    for i in range(len(missile_list)):
+        for j in range(len(rock_list)):
+            m = missile_list[i]
+            r = rock_list[j]
+            if crash(m, r) == True:
+                dm_list.append(i)
+                dr_list.append(j)
+    dm_list = list(set(dm_list))
+    dr_list = list(set(dr_list))
+
+    for dm in dm_list:
+        del missile_list[dm]
+    for dr in dr_list:
+        del rock_list[dr]
+        kill += 1
+    
+    for i in range(len(rock_list)):
+        r = rock_list[i]
+        if crash(r, fighter) == True:
+            running = False
+            GO = 1
+
 
 
 
@@ -138,7 +198,23 @@ while running:
         m.show()
     for m in rock_list:
         m.show()    
+
+    font = pygame.font.Font("C:/Windows/Fonts/ariblk.ttf", 20)
+    text_kill = font.render("killed : {} loss: {}".format(kill, loss), True, (255, 255, 0))
+    screen.blit(text_kill, (10, 5))
+
+    text_time = font.render("time : {}".format(delta_time), True, (255, 255, 255))
+    screen.blit(text_time, (SCREEN_WIDTH - 100, 5))
+
     # 4-5. 업데이트
     pygame.display.flip()
 # 5. 게임 종료
+while GO == 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GO = 0
+    font = pygame.font.Font("C:/Windows/Fonts/ariblk.ttf", 40)
+    text = font.render("GAME OVER", True, (255, 0, 0))
+    screen.blit(text, (80, SCREEN_HEIGHT / 2 - 50))
+    pygame.display.flip()
 pygame.quit()
